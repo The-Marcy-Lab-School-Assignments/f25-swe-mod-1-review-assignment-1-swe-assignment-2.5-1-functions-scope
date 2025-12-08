cf429072dd5efe147bd8dd454d3b5baa5d95c0d5
tylekmj-ops <tylekmj@gmail.com>
2025-12-08 08:49:27 -0500

    first commit

--- src/1-greet-with-excitement.js
+++ src/1-greet-with-excitement.js
@@ -1,14 +1,17 @@
 const createGreeting = (name) => {
   // Your code here
-}
+  return `Hello, ${name}`;
+};
 
 const addExcitement = (message) => {
   // Your code here
-}
+  return `${message}!!`;
+};
 
 const greetWithExcitement = (name) => {
   // Your code here - use the two functions above
-}
+  return `${createGreeting(name)}${addExcitement(``)}`;
+};
 
 console.log(createGreeting("Alex")); // Should print: "Hello, Alex"
 console.log(addExcitement("Alex")); // Should print: "Alex!!"
--- src/3-mask-email.js
+++ src/3-mask-email.js
@@ -1,6 +1,14 @@
 const maskEmail = (email) => {
+  email = email.trim();
 
-}
+  let symbolIndex = email.indexOf("@");
+  if (symbolIndex == 1) {
+    return email;
+  }
+
+  let domain = email.slice(symbolIndex);
+  return `${email[0]}***${domain}`;
+};
 
 console.log(maskEmail("maya.rodriguez@example.com"));
 // "m***@example.com"
